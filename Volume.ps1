ba9b26bdafdc670c34a5a00f063ef7a158dcaed0
Falcon <101268424+Falconnix@users.noreply.github.com>
2023-04-28 13:49:18 -0400

    Update Volume.ps1

--- Volume.ps1
+++ Volume.ps1
@@ -1,1 +1,1 @@
-while (1) {$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}}
+while (1) {Start-Sleep -Seconds 2;$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}}

